6456b9acd7d8e5397212b0b0b5e556e3cb9f2dda
NorthWestWind <wsyn148@gmail.com>
2022-06-04 16:51:49 +0800

    untested

---
+++ public/scripts/js/types/entities.ts
@@ -0,0 +1,23 @@
+import { CircleHitbox, Hitbox, Vec2 } from "./maths";
+
+export class Entity {
+	type: string = "";
+	position: Vec2;
+	velocity: Vec2 = Vec2.ZERO;
+	direction: Vec2 = Vec2.ONE;
+	hitbox: Hitbox = CircleHitbox.ZERO;
+}
+
+export class Player extends Entity {
+	type = "player";
+	id: string;
+	health: number = 100;
+	boost: number = 1;
+	scope: number = 1;
+}
+
+export class Bullet extends Entity {
+	type = "bullet";
+	damage: number;
+	ticks: number;
+}
---
+++ public/scripts/js/types/maths.ts
@@ -0,0 +1,107 @@
+// Calculus paid off!
+export class Vec2 {
+	static readonly ZERO = new Vec2(0, 0);
+	static readonly ONE = new Vec2(1, 0);
+
+	readonly x: number;
+	readonly y: number;
+
+	constructor(x: number, y: number) {
+		this.x = x;
+		this.y = y;
+	}
+
+	magnitudeSqr() {
+		return this.x * this.x + this.y * this.y;
+	}
+
+	magnitude() {
+		return Math.sqrt(this.magnitudeSqr());
+	}
+
+	inverse() {
+		return new Vec2(-this.x, -this.y);
+	}
+
+	unit() {
+		const mag = this.magnitude();
+		return new Vec2(this.x / mag, this.y / mag);
+	}
+
+	dot(vec: Vec2) {
+		return this.x * vec.x + this.y * vec.y;
+	}
+
+	angleBetween(vec: Vec2) {
+		return Math.acos(this.dot(vec) / (this.magnitude() * vec.magnitude()));
+	}
+
+	angle() {
+		// Magnitude of unit vector is 1
+		return Math.acos((this.x) / (this.magnitude()));
+	}
+
+	addAngle(radian: number) {
+		const angle = this.angle() + radian;
+		const mag = this.magnitude();
+		return new Vec2(mag * Math.cos(angle), mag * Math.sin(angle));
+	}
+
+	addVec(vec: Vec2) {
+		return new Vec2(this.x + vec.x, this.y + vec.y);
+	}
+
+	addX(x: number) {
+		return new Vec2(this.x + x, this.y);
+	}
+
+	addY(y: number) {
+		return new Vec2(this.x, this.y + y);
+	}
+
+	scale(x: number, y: number) {
+		return new Vec2(this.x * x, this.y * y);
+	}
+
+	scaleAll(ratio: number) {
+		return this.scale(ratio, ratio);
+	}
+}
+
+export interface Hitbox {
+	type: "rect" | "circle";
+}
+
+// Rectangle hitbox with a width and height
+export class RectHitbox implements Hitbox {
+	static readonly ZERO = new RectHitbox(0, 0);
+
+	type: "rect" = "rect";
+	width: number;
+	height: number;
+
+	constructor(width: number, height: number) {
+		this.width = width;
+		this.height = height;
+	}
+}
+
+// Circle hitbox with a radius
+export class CircleHitbox implements Hitbox {
+	static readonly ZERO = new RectHitbox(0, 0);
+
+	type: "circle" = "circle";
+	radius: number;
+
+	constructor(radius: number) {
+		this.radius = radius;
+	}
+}
+
+// The 4 movement directions
+export enum MovementDirection {
+	RIGHT = 0,
+	UP = 1,
+	LEFT = 2,
+	DOWN = 3
+}
