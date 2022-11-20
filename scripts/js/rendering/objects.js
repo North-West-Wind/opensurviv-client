b421c285dabd5f1df4f933a7738fd361eba9241c
Viswas <viswas.anand@outlook.com>
2022-11-20 11:57:04 +0530

    Update objects.js
    Trying to add a barrel object (will upload image soon)

--- scripts/js/rendering/objects.js
+++ scripts/js/rendering/objects.js
@@ -1,4 +1,6 @@
 const treeImg = new Image();
+const barrelImg = new Image();
+barrelImg.onload = () => barrelImg.loaded = true;
 treeImg.onload = () => treeImg.loaded = true;
 treeImg.src = "assets/images/tree.svg";
 
@@ -10,10 +12,21 @@ treeImg.src = "assets/images/tree.svg";
  * @param {CanvasRenderingContext2D} ctx 
  * @param {number} scale 
  */
+/**
+ * Draws a barrel
+ * @param {Player} you
+ * @param {Barrel} barrel
+ * @param {HTMLCanvasElement} canvas
+ * @param {CanvasRenderingContext2D} ctx
+ * @param {number} scale2
+*/
 function drawTree(you, tree, canvas, ctx, scale) {
 	if (!treeImg.loaded) return;
 	const relative = tree.position.addVec(you.position.inverse());
 	const width = scale * tree.hitbox.comparable() * 10, height = width * treeImg.naturalWidth / treeImg.naturalHeight;
 	const x = (canvas.width - width) / 2 + relative.x * scale, y = (canvas.height - height) / 2 + relative.y * scale;
 	ctx.drawImage(treeImg, x, y, width, height);
+}
+function drawBarrel(you, barrel, canvas, ctx, scale2){
+
 }
