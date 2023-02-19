03d69bb5035e650ee40ea3aa9faa4824fc7902be
Viswas <viswas.anand@outlook.com>
2023-02-19 13:28:39 +0530

    Added soviet crates
    советские ящики! они сбросят 2 пушки. m870 и mp5 будут сброшены из ящика! На карте будет порождено 14 советских ящиков.
    Jk, because it's 'soviet' crate i translated english to russian!
    
    I added soviet crates! these will drop 2 guns . m870 and mp5 will be dropped from the crate!
    There will be 14 soviet crates spawned in the map

--- src/store/obstacles/index.ts
+++ src/store/obstacles/index.ts
@@ -6,12 +6,14 @@ import Crate from "./crate";
 import Stone from "./stone";
 import Tree from "./tree";
 import mosin_Tree from "./mosin_tree";
+import soviet_Crate from "./soviet_crate";
 
 export { default as Tree } from "./tree";
 export { default as Bush } from "./bush";
 export { default as Crate } from "./crate";
 export { default as mosin_Tree} from "./mosin_tree";
 export { default as Stone } from "./stone";
+export { default as soviet_Crate} from "./soviet_crate";
 
 // This still need hard-coding unfortunately
 export function castCorrectObstacle(minObstacle: MinObstacle & any) {
@@ -25,7 +27,9 @@ export function castCorrectObstacle(minObstacle: MinObstacle & any) {
 		case "stone":
 			return new Stone(minObstacle);
 		case "mosin_tree":
-			return new mosin_Tree(minObstacle)
+			return new mosin_Tree(minObstacle);
+		case "soviet_crate":
+			return new soviet_Crate(minObstacle);
 		default:
 			return new DummyObstacle(minObstacle);
 	}
