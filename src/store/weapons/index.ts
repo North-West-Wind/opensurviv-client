0c8bf8d30a9bfe659c914ed873eb2d82eee11baa
Viswas <viswas.anand@outlook.com>
2023-03-08 10:26:48 +0530

    Rendering more guns!
    Added gun renderers for M870, Mosin nagant and MP5!
    
    Keep in mind that my PR here doesn't include the updated compiled binaries  (bundle.min.js), so, to use this, you need to pull it in your system, then compile it, then run it!

--- src/store/weapons/index.ts
+++ src/store/weapons/index.ts
@@ -7,6 +7,9 @@ import Fists from "./fists";
 
 export { default as Fists } from "./fists";
 export { default as M9 } from "./guns/m9";
+export { default as M870} from "./guns/m870";
+export { default as Mosin_Nagant} from "./guns/mosin_nagant";
+export { default as MP5} from "./guns/mp5";
 
 export function castCorrectWeapon(minWeapon: MinWeapon & any) {
 	if (WEAPON_SUPPLIERS.has(minWeapon.id)) return WEAPON_SUPPLIERS.get(minWeapon.id)!.create(minWeapon);
