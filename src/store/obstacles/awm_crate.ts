145479519c6431f44b068c94541cb5454a540847
Viswas <viswas.anand@outlook.com>
2023-03-14 22:29:35 +0530

    AWM Crate strech fix?
    Overstrching fix?

--- src/store/obstacles/awm_crate.ts
+++ src/store/obstacles/awm_crate.ts
@@ -33,14 +33,14 @@ export default class AWMCrate extends Obstacle {
 		const width = scale * (<RectHitbox>this.hitbox).width * (this.despawn ? 0.5 : 1), height = width * awmCrateImg.naturalWidth / awmCrateImg.naturalHeight;
 		ctx.translate(canvas.width / 2 + relative.x * scale, canvas.height / 2 + relative.y * scale);
 		ctx.rotate(-this.direction.angle());
-		ctx.drawImage(this.despawn ? awmCrateResidueImg : awmCrateImg, -width / 2, -height / 2, width, height);
+		ctx.drawImage(this.despawn ? awmCrateResidueImg : awmCrateImg, -width / 2, -height / 2, width + 1, height + 0.5);
 		ctx.resetTransform();
 	}
 
 	renderMap(_canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D, scale: number) {
 		ctx.translate(this.position.x * scale, this.position.y * scale);
 		ctx.fillStyle = "#683c05";
-		ctx.fillRect(-2 * scale, -2 * scale, 16 * scale, 6 * scale);
+		ctx.fillRect(-2 * scale, -2 * scale, 5 * scale, 4 * scale);
 		ctx.resetTransform();
 	}
 }
